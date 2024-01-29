d61759c6add7bc200f4139a9f67c98db05ff26cb
Gourav Gupta <gouravgupta7799@gmail.com>
2024-01-29 16:24:23 +0530

    .

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,28 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Expenses.module.css'
 
 export default function Expenses(props) {
+
+
+  const [isTheme, setIsTheme] = useState(false)
+  
+  const totalAmount =props.items.reduce((accumulator, expense) => {
+    return accumulator + Number(expense.amount);
+  }, 0);
+
   return (
-    <>
-      <div className={classes.main}>
-        <h2 className={classes.header}> Expnse list</h2>
-        {props.items.length === 0 ? <p>no expnse add yet</p> :
-          (
-            <ul className={classes['table-list']}>
-              {props.items.map((expense) => (
-                <li key={expense.id} className={classes.li}>
-                  <p><strong>{expense.description}</strong></p> - <p>${expense.amount}</p> <p>({expense.category})</p>
-                  <li className={classes['inner-li']}>
-                    <button className={classes['editBtn']} onClick={() => { }}>Edit</button>
-                    <button className={classes['deleteBtn']} onClick={() => { }}>X</button>
-                  </li>
-                </li>
+    <React.Fragment><div className={`${classes.main} ${isTheme ? classes.dark : ''}`}>
+      <h2 className={classes.header}> Expnse list</h2>
+      {props.items.length === 0 ? <p>no expnse add yet</p> :
+        (
+          <ul className={classes.ul}>
+            {props.items.map((expense) => (
+              <li key={expense.id} className={`${classes.li}  ${isTheme ? classes.dark : ''}`}>
+                <div className={classes.box}>{expense.description}</div>
+                <div className={classes.boxs}><p>{expense.amount}₹</p>
+                  <p>{expense.category} </p></div>
+                <div className={classes.buttons}>
+                  <button onClick={() => { }} className={classes.edit}>Edit</button>
+                  <button onClick={() => { }} className={classes.delete}>Delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      {props.items.length > 0 &&
+        <div className={classes.downloadBtn}>
+          <button className={classes.btn} onClick={() => { }} >Download csv</button>
+        </div>
+      }
 
-              ))}
-            </ul>
-          )}
+    </div>
 
-      </div>
-    </>
+      <span className={`${classes.sidebar}  ${isTheme ? classes.dark : ''}`}>
+        <h3 className={classes.sideHeading}>Total Amount</h3>
+        <h1 className={classes.totalAmount}> {totalAmount}₹</h1>
+        {totalAmount > 1000 && <button className={classes.newBtn} onClick={() => { }}>Active Premium</button>}
+      </span>
+    </React.Fragment>
   )
 }
